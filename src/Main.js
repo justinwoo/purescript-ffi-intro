f29a875ca81f7671ea97f0dc25438bf4a7419647
justinwoo <moomoowoo@gmail.com>
2018-08-05 01:23:31 +0200

    updates from session

--- src/Main.js
+++ src/Main.js
@@ -5,7 +5,24 @@ exports._log = function(str) {
   };
 };
 
+exports._log2 = function(str1) {
+  return function(str2) {
+    return function() {
+      console.log(str1, str2);
+    };
+  };
+};
+
 // uncurried function of 1 argument which runs the effect directly
 exports.__log = function(str) {
   console.log(str);
+};
+
+exports.__log2 = function(str1, str2) {
+  console.log(str1, str2);
+  return 123;
+};
+
+exports.__log3 = function(foreign) {
+  console.log(foreign);
 };
--- src/Main.purs
+++ src/Main.purs
@@ -8,12 +8,31 @@ module Main
 
 import Prelude
 
+import Data.Either (Either(..))
 import Effect (Effect)
 import Effect.Uncurried as EU
+import Foreign (Foreign, unsafeToForeign)
+import Simple.JSON as JSON
 
 foreign import _log :: String -> Effect Unit
 
-foreign import __log :: EU.EffectFn1 String Unit
+foreign import _log2 :: String -> String -> Effect Unit
+
+foreign import __log :: forall a. EU.EffectFn1 a Unit
+
+foreign import __log2 :: EU.EffectFn2 String String Foreign
+
+foreign import __log3 :: EU.EffectFn1 Foreign Unit
+
+callLog3 :: forall a. a -> Effect Unit
+callLog3 = EU.runEffectFn1 __log3 <<< unsafeToForeign
+
+callLog2AndDecodeIt :: Effect Unit
+callLog2AndDecodeIt = do
+  blob <- EU.runEffectFn2 __log2 "asdf" "zxcv"
+  case JSON.read blob of
+    Right (x :: Int) -> log "Did this correctly"
+    Left e -> log $ "Did this wrong: " <> show e
 
 -- expose a normal PureScript function for the uncurried function
 log :: String -> Effect Unit
@@ -21,6 +40,7 @@ log = EU.runEffectFn1 __log
 
 main :: Effect Unit
 main = do
+  callLog2AndDecodeIt
   _log "Hello from PureScript"
   log "Hello from PureScript 2"
 
